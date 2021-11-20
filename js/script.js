524bb1d18d56ddd9fcb715fc6da03d2450bc52f9
Vlad Crauciuc <vlad.crauciuc1@gmail.com>
2021-11-20 16:39:13 +0200

    _v2

--- js/script.js
+++ js/script.js
@@ -3,33 +3,31 @@ const form = document.getElementById('inputForm');
 const inputField = document.getElementById('inputField');
 const messageSpan = document.getElementById('messageSpan');
 const messageContainer = document.getElementById('messageContainer');
-const messageType = document.getElementById('messageType')
+const messageType = document.getElementById('messageType');
+const restartButton = document.getElementById('restartButton');
 
 form.addEventListener('submit', (event) => {
     let message = '';
     if (Number(inputField.value) === generatedNumber) {
-        message = 'This is the number. Play again? (refresh page)';
+        message = 'This is the number. Play again?';
         messageContainer.classList.remove('d-none');
         messageContainer.classList.add('d-flex');
         messageType.classList.remove('alert-warning');
         messageType.classList.add('alert-success');
+        restartButton.classList.remove('d-none');
     } else if (Number(inputField.value) > generatedNumber) {
         message = 'The number is smaller than your guess. Try again';
         messageContainer.classList.add('d-flex');
         messageContainer.classList.remove('d-none');
-        setTimeout(() => {
-            messageContainer.classList.remove('d-flex');
-            messageContainer.classList.add('d-none');
-        }, 1500)
     } else {
         message = 'The number is greater than your guess. Try again';
         messageContainer.classList.add('d-flex');
         messageContainer.classList.remove('d-none');
-        setTimeout(() => {
-            messageContainer.classList.remove('d-flex');
-            messageContainer.classList.add('d-none');
-        }, 1500)
     }
     messageSpan.innerText = message;
     event.preventDefault();
 })
+
+restartButton.addEventListener('click', () => {
+    window.location.reload();
+});
