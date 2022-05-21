25d6aee7f88cc3d94469ea073406ba6a5225978d
Vlad Crauciuc <vlad.crauciuc1@gmail.com>
2022-05-21 22:49:47 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// const check = document.querySelector('.form-check-input');
+// const leaderWeaponsRow = document.querySelector('.leader-weapons-row');
+
+// check.addEventListener('change', () => {
+//   leaderWeaponsRow.classList.toggle('d-none');
+// });
